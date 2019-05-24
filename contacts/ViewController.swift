80344773a98916ac812eb82437527052f5fe7350
Веригин С.И <si.verigin@usd-macprog75.local>
2019-05-24 10:26:04 +0300

    Initial Commit

---
+++ contacts/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  contacts
+//
+//  Created by Веригин С.И. on 24/05/2019.
+//  Copyright © 2019 Веригин С.И. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
